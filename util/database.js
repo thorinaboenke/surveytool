1eeb1ceabbe6f3109a41214ae213a4fc709796ad
Thorina Boenke <thorina-boenke@web.de>
2020-12-04 18:02:26 +0100

    Create components

---
+++ util/database.js
@@ -0,0 +1,4 @@
+import postgres from 'postgres';
+import dotenv from 'dotenv';
+import camelcaseKeys from 'camelcase-keys';
+import extractHerokuDatabaseEnvVars from './extractHerokuDatabaseEnvVars';
