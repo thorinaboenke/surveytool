a2b98722c0c89a8e61a59c025d031085d07200e6
Thorina Boenke <thorina-boenke@web.de>
2020-12-05 18:45:21 +0100

    remove couble var declaration

--- util/database.ts
+++ util/database.ts
@@ -13,15 +13,6 @@ const sql =
       // has an "unauthorized" certificate
       // https://devcenter.heroku.com/changelog-items/852
       postgres({ ssl: { rejectUnauthorized: false } })
-    : postgres({ idle_timeout: 5 });
-dotenv.config();
-
-const sql =
-  process.env.NODE_ENV === 'production'
-    ? // Heroku needs SSL connections but
-      // has an "unauthorized" certificate
-      // https://devcenter.heroku.com/changelog-items/852
-      postgres({ ssl: { rejectUnauthorized: false } })
     : postgres();
 
 export async function getUserByUsername(username: string) {
