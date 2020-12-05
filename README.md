9ea370a9122f0542120f708545503ebd39522be3
Thorina Boenke <thorina-boenke@web.de>
2020-12-05 18:43:17 +0100

    extract env vars, upgrade postgres

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # Survey Tool MVP
 
-I made this app
+This app got created as a coding challenge during a weekend following a job interview for a developer position.
+
+## Choose a task
+
+## Roadmap
+
+## Database Schema
+
+## App structure
+
+## API routes
+
+## Pages
+
+## Deploying
+
+## Styling
 
 ## Lessons learned
--- util/database.ts
+++ util/database.ts
@@ -4,20 +4,16 @@ import camelcaseKeys from 'camelcase-keys';
 import extractHerokuDatabaseEnvVars from './extractHerokuDatabaseEnvVars';
 import { User, Session, Survey, Result, Question, Answer } from './types';
 
-module.exports = function setPostgresDefaultsOnHeroku() {
-  if (process.env.DATABASE_URL) {
-    const { parse } = require('pg-connection-string');
+extractHerokuDatabaseEnvVars();
+dotenv.config();
 
-    // Extract the connection information from the Heroku environment variable
-    const { host, database, user, password } = parse(process.env.DATABASE_URL);
-
-    // Set standard environment variables
-    process.env.PGHOST = host;
-    process.env.PGDATABASE = database;
-    process.env.PGUSERNAME = user;
-    process.env.PGPASSWORD = password;
-  }
-};
+const sql =
+  process.env.NODE_ENV === 'production'
+    ? // Heroku needs SSL connections but
+      // has an "unauthorized" certificate
+      // https://devcenter.heroku.com/changelog-items/852
+      postgres({ ssl: { rejectUnauthorized: false } })
+    : postgres({ idle_timeout: 5 });
 dotenv.config();
 
 const sql =
