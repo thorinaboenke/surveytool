d0a70d132b4c380ed3f3fb7f894c710d398e25e3
Thorina Boenke <thorina-boenke@web.de>
2020-12-05 18:26:56 +0100

    set Postgres defaults on heroku

--- util/database.ts
+++ util/database.ts
@@ -4,7 +4,20 @@ import camelcaseKeys from 'camelcase-keys';
 import extractHerokuDatabaseEnvVars from './extractHerokuDatabaseEnvVars';
 import { User, Session, Survey, Result, Question, Answer } from './types';
 
-extractHerokuDatabaseEnvVars();
+module.exports = function setPostgresDefaultsOnHeroku() {
+  if (process.env.DATABASE_URL) {
+    const { parse } = require('pg-connection-string');
+
+    // Extract the connection information from the Heroku environment variable
+    const { host, database, user, password } = parse(process.env.DATABASE_URL);
+
+    // Set standard environment variables
+    process.env.PGHOST = host;
+    process.env.PGDATABASE = database;
+    process.env.PGUSERNAME = user;
+    process.env.PGPASSWORD = password;
+  }
+};
 dotenv.config();
 
 const sql =
